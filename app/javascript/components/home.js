1fd2de178ad8897b417540c7a7d90b5184615715
Travis Baker <travi5jab@traviss-mbp.attlocal.net>
2019-07-22 20:43:04 -0700

    test

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -139,7 +139,7 @@ class Home extends React.Component {
                 <p>this button does not work currently</p>
                 <Button>{current_user && "Confirm Location" || "Hide my Location"}</Button>
                 </div>
-
+                <div></div>
                 </div>
             </div>
         </div>
